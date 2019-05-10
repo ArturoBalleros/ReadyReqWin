e95403c801b61dc4059e164f661c8cf00b25c6b5
ArturoBalleros <karveg7@gmail.com>
2019-05-10 12:35:15 +0200

    Limpieza de código en menu

---
+++ ReadyReq/Util/ClsDefVal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadyReq.Util
+{
+    public struct Español
+    {
+        public const string Menu = "Menú";
+        public const string Grupo_Trabajo = "Grupo de trabajo";
+        public const string Paquetes = "Paquetes";
+        public const string Objetivos = "Objetivos";
+        public const string Actores = "Actores";
+        public const string ReqInfo = "Requisitos de Información";
+        public const string ReqFun = "Requisitos Funcionales";
+        public const string ReqNFun = "Requisitos no Funcionales";
+        public const string Configuración = "Configuración";
+        public const string Nue_Proy = "Nuevo Proyecto";
+        public const string Importar_BD = "Importar a Base de Datos";
+        public const string Exportar_Fich = "Exportar a Fichero";
+        public const string Esportar_Word = "Exportar a Word";
+        public const string Estimaciones = "Estimaciones";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+
+    }
+
+    public struct Ingles
+    {
+        public const string Menu = "Menu";
+        public const string Workgroup = "Workgroup";
+        public const string Packages = "Packages";
+        public const string Objectives = "Objectives";
+        public const string Actors = "Actors";
+        public const string ReqInfo = "Information Requirements";
+        public const string ReqFun = "Functional Requirements";
+        public const string ReqNFun = "Non-functional requirements";
+        public const string Configuration = "Configuration";
+        public const string New_Proj = "New Project";
+        public const string Import_DB = "Import to Database";
+        public const string Export_File = "Export to File";
+        public const string Export_Word = "Export to Word";
+        public const string Estimates = "Estimates";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+    }
+
+    public struct DefValues
+    {
+        public const string Ingles = "Ingles";
+       // public const string Español = "Paquetes";
+        public const string MySql = "MySql";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+        //public const string aaa = "Arturo";
+    }
+}
--- ReadyReq/ViewModel/WinMenu.xaml.cs
+++ ReadyReq/ViewModel/WinMenu.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using ReadyReq.Model;
-using ReadyReq.ViewModel;
+using ReadyReq.Util;
 using System;
 using System.Windows;
 using System.Windows.Controls;
@@ -49,165 +49,142 @@ namespace ReadyReq.ViewModel
         }
         private void Idioma()
         {
-            Title = "Menu";
-            if (ClsConf.Idioma == "Ingles")
+            if (ClsConf.Idioma == DefValues.Ingles)
             {
+                Title = Ingles.Menu;
+
                 //Label
-                TxtGru.Text = "Workgroup";
-                TxtPaq.Text = "Packages";
-                TxtObj.Text = "Objectives";
-                TxtAct.Text = "Actors";
-                TxtRIn.Text = "Information Requirements";
-                TxtRFu.Text = "Functional Requirements";
-                TxtRNF.Text = "Non-functional requirements";
-                TxtCon.Text = "Configuration";
-                TxtNuP.Text = "New Project";
-                TxtImp.Text = "Import to Database";
-                TxtExp.Text = "Export to File";
-                TxtWord.Text = "Export to Word";
-                TxtEstim.Text = "Estimates";
+                TxtGru.Text = Ingles.Workgroup;
+                TxtPaq.Text = Ingles.Packages;
+                TxtObj.Text = Ingles.Objectives;
+                TxtAct.Text = Ingles.Actors;
+                TxtRIn.Text = Ingles.ReqInfo;
+                TxtRFu.Text = Ingles.ReqFun;
+                TxtRNF.Text = Ingles.ReqNFun;
+                TxtCon.Text = Ingles.Configuration;
+                TxtNuP.Text = Ingles.New_Proj;
+                TxtImp.Text = Ingles.Import_DB;
+                TxtExp.Text = Ingles.Export_File;
+                TxtWord.Text = Ingles.Export_Word;
+                TxtEstim.Text = Ingles.Estimates;
             }
             else
             {
+                Title = Español.Menu;
+
                 //Label
-                TxtGru.Text = "Grupo de trabajo";
-                TxtPaq.Text = "Paquetes";
-                TxtObj.Text = "Objetivos";
-                TxtAct.Text = "Actores";
-                TxtRIn.Text = "Requisitos de Información";
-                TxtRFu.Text = "Requisitos Funcionales";
-                TxtRNF.Text = "Requisitos no Funcionales";
-                TxtCon.Text = "Configuración";
-                TxtNuP.Text = "Nuevo Proyecto";
-                TxtImp.Text = "Importar a Base de Datos";
-                TxtExp.Text = "Exportar a Fichero";
-                TxtWord.Text = "Exportar a Word";
-                TxtEstim.Text = "Estimaciones";
+                TxtGru.Text = Español.Grupo_Trabajo;
+                TxtPaq.Text = Español.Paquetes;
+                TxtObj.Text = Español.Objetivos;
+                TxtAct.Text = Español.Actores;
+                TxtRIn.Text = Español.ReqInfo;
+                TxtRFu.Text = Español.ReqFun;
+                TxtRNF.Text = Español.ReqNFun;
+                TxtCon.Text = Español.Configuración;
+                TxtNuP.Text = Español.Nue_Proy;
+                TxtImp.Text = Español.Importar_BD;
+                TxtExp.Text = Español.Exportar_Fich;
+                TxtWord.Text = Español.Esportar_Word;
+                TxtEstim.Text = Español.Estimaciones;
             }
         }
         private void Click(object sender, RoutedEventArgs e)
         {
             ctrl = ((Control)sender);
-            if (ctrl.Name == "ButGru")
+            if (ctrl.Name.Equals("ButGru") && !Grupo.IsLoaded && Activo)
             {
-                if (Grupo.IsLoaded == false && Activo == true)
-                {
-                    Grupo = new WinGrupo();
-                    Grupo.Owner = this;
-                    Grupo.Show();
-                }
+                Grupo = new WinGrupo();
+                Grupo.Owner = this;
+                Grupo.Show();
             }
-            if (ctrl.Name == "ButPaq")
+
+            if (ctrl.Name.Equals("ButPaq") && !Paquetes.IsLoaded && Activo)
             {
-                if (Paquetes.IsLoaded == false && Activo == true)
-                {
-                    Paquetes = new WinPaq();
-                    Paquetes.Owner = this;
-                    Paquetes.Show();
-                }
+                Paquetes = new WinPaq();
+                Paquetes.Owner = this;
+                Paquetes.Show();
             }
-            if (ctrl.Name == "ButObj")
+
+            if (ctrl.Name.Equals("ButObj") && !Objetivos.IsLoaded && Activo)
             {
-                if (Objetivos.IsLoaded == false && Activo == true)
-                {
-                    Objetivos = new WinObjetivos();
-                    Objetivos.Owner = this;
-                    Objetivos.Show();
-                }
+                Objetivos = new WinObjetivos();
+                Objetivos.Owner = this;
+                Objetivos.Show();
             }
-            if (ctrl.Name == "ButAct")
+
+            if (ctrl.Name.Equals("ButAct") && !Actores.IsLoaded && Activo)
             {
-                if (Actores.IsLoaded == false && Activo == true)
-                {
-                    Actores = new WinActores();
-                    Actores.Owner = this;
-                    Actores.Show();
-                }
+                Actores = new WinActores();
+                Actores.Owner = this;
+                Actores.Show();
             }
-            if (ctrl.Name == "ButRIn")
+
+            if (ctrl.Name.Equals("ButRIn") && !ReqInfo.IsLoaded && Activo)
             {
-                if (ReqInfo.IsLoaded == false && Activo == true)
-                {
-                    ReqInfo = new WinReqInfo();
-                    ReqInfo.Owner = this;
-                    ReqInfo.Show();
-                }
+                ReqInfo = new WinReqInfo();
+                ReqInfo.Owner = this;
+                ReqInfo.Show();
             }
-            if (ctrl.Name == "ButRFu")
+
+            if (ctrl.Name.Equals("ButRFu") && !ReqFun.IsLoaded && Activo)
             {
-                if (ReqFun.IsLoaded == false && Activo == true)
-                {
-                    ReqFun = new WinReqFun();
-                    ReqFun.Owner = this;
-                    ReqFun.Show();
-                }
+                ReqFun = new WinReqFun();
+                ReqFun.Owner = this;
+                ReqFun.Show();
             }
-            if (ctrl.Name == "ButRNF")
+
+            if (ctrl.Name.Equals("ButRNF") && !ReqNFun.IsLoaded && Activo)
             {
-                if (ReqNFun.IsLoaded == false && Activo == true)
-                {
-                    ReqNFun = new WinReqNFun();
-                    ReqNFun.Owner = this;
-                    ReqNFun.Show();
-                }
+                ReqNFun = new WinReqNFun();
+                ReqNFun.Owner = this;
+                ReqNFun.Show();
             }
-            if (ctrl.Name == "ButCon")
+
+            if (ctrl.Name.Equals("ButCon") && !Configuracion.IsLoaded)
             {
-                if (Configuracion.IsLoaded == false)
-                {
-                    Configuracion = new WinConf();
-                    Configuracion.Owner = this;
-                    Configuracion.ShowDialog();
-                    Activo = true;
-                    if (ClsConf.TipoBD == "MySql") ClsConf.ConexionMySql();
-                    Idioma();
-                }
+                Configuracion = new WinConf();
+                Configuracion.Owner = this;
+                Configuracion.ShowDialog();
+                Activo = true;
+                if (ClsConf.TipoBD == DefValues.MySql) ClsConf.ConexionMySql();
+                Idioma();
             }
-            if (ctrl.Name == "ButNuP")
+
+            if (ctrl.Name.Equals("ButNuP") && !NuevoPro.IsLoaded && Activo)
             {
-                if (NuevoPro.IsLoaded == false && Activo == true)
-                {
-                    NuevoPro = new WinNuePro();
-                    NuevoPro.Owner = this;
-                    NuevoPro.ShowDialog();
-                }
+                NuevoPro = new WinNuePro();
+                NuevoPro.Owner = this;
+                NuevoPro.ShowDialog();
             }
-            if (ctrl.Name == "ButImp")
+
+            if (ctrl.Name.Equals("ButImp") && !Importar.IsLoaded && Activo)
             {
-                if (Importar.IsLoaded == false && Activo == true)
-                {
-                    Importar = new WinImpor();
-                    Importar.Owner = this;
-                    Importar.ShowDialog();
-                }
+                Importar = new WinImpor();
+                Importar.Owner = this;
+                Importar.ShowDialog();
             }
-            if (ctrl.Name == "ButExp")
+
+            if (ctrl.Name.Equals("ButExp") && !Exportar.IsLoaded && Activo)
             {
-                if (Exportar.IsLoaded == false && Activo == true)
-                {
-                    Exportar = new WinExpor();
-                    Exportar.Owner = this;
-                    Exportar.ShowDialog();
-                }
+                Exportar = new WinExpor();
+                Exportar.Owner = this;
+                Exportar.ShowDialog();
             }
-            if (ctrl.Name == "ButWord")
+
+            if (ctrl.Name.Equals("ButWord") && !Word.IsLoaded && Activo)
             {
-                if (Word.IsLoaded == false && Activo == true)
-                {
-                    Word = new WinWord();
-                    Word.Owner = this;
-                    Word.ShowDialog();
-                }
+                Word = new WinWord();
+                Word.Owner = this;
+                Word.ShowDialog();
             }
-            if (ctrl.Name == "ButEstim")
+
+            if (ctrl.Name.Equals("ButEstim") && !Estim.IsLoaded && Activo)
             {
-                if (Estim.IsLoaded == false && Activo == true)
-                {
-                    Estim = new WinEstim();
-                    Estim.Owner = this;
-                    Estim.ShowDialog();
-                }
+                Estim = new WinEstim();
+                Estim.Owner = this;
+                Estim.ShowDialog();
             }
+
         }
         private void WClosed(object sender, EventArgs e)
         {
